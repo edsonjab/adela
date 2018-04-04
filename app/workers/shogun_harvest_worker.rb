444ad4ae00531d16f8791360e020427f01734083
Edson Amaya <edsonjab@gmail.com>
2018-04-04 10:55:44 -0500

    fix parameter to method updateorg on shogunharvestworker

--- app/workers/shogun_harvest_worker.rb
+++ app/workers/shogun_harvest_worker.rb
@@ -5,7 +5,7 @@ class ShogunHarvestWorker
     ShogunService.harvest(catalog_url)
   end
 
-  def updateorg(catalog_url)
+  def updateorg(oldname, newname)
     ShogunService.updateorg(oldname, newname)
   end
 
