fd4dde72249d2c75d4c912d58ec914fdebe690a6
KielRodriguez <kr.rdz.20@gmail.com>
2018-04-04 14:03:27 -0500

    create test to updateor endpoint

--- config/initializers/shogun.rb
+++ config/initializers/shogun.rb
@@ -1,1 +1,2 @@
-SHOGUN_BASE_URI = ENV['SHOGUN_BASE_URI'] || 'shogun-staging.herokuapp.com'
+# SHOGUN_BASE_URI = ENV['SHOGUN_BASE_URI'] || 'shogun-staging.herokuapp.com'
+SHOGUN_BASE_URI = ENV['SHOGUN_BASE_URI'] || '10.20.55.7/shogun'
